3ca9174ba4a5307f67fc4140a864108aae1c8481
Michael Hendricks <michael@ndrix.org>
2013-05-03 15:09:51 -0600

    ECDH using Bitcoin's curve
    This is the code from my earlier Gist
    (https://gist.github.com/mndrix/5373519) split into library code and
    an example executable.
    
    The main purpose is to make secp256k1 curve parameters available to
    other code that might want to use it.

---
+++ examples/ecdh.go
@@ -0,0 +1,23 @@
+package main
+
+import "crypto/ecdsa"
+import "crypto/rand"
+import "fmt"
+import "github.com/mndrix/btcutil"
+
+func main() {
+	// generate keys for Alice and Bob
+	alice, _ := ecdsa.GenerateKey(btcutil.Secp256k1, rand.Reader)
+	bob, _ := ecdsa.GenerateKey(btcutil.Secp256k1, rand.Reader)
+	fmt.Printf("Alice:\t%x\n\t%x\n", alice.D, alice.PublicKey.X)
+	fmt.Printf("Bob:\t%x\n\t%x\n", bob.D, bob.PublicKey.X)
+	fmt.Println("")
+
+	// Alice calculates shared secret
+	aliceShared := btcutil.ECDH(alice, bob.PublicKey)
+	fmt.Printf("Alice: %x\n", aliceShared)
+
+	// Bob calculates shared secret
+	bobShared := btcutil.ECDH(bob, alice.PublicKey)
+	fmt.Printf("Bob:   %x\n", bobShared)
+}
---
+++ secp256k1.go
@@ -0,0 +1,32 @@
+package btcutil
+
+import "crypto/ecdsa"
+import "crypto/elliptic"
+import "fmt"
+import "math/big"
+
+// secp256k1 curve parameters.  Only available after init() has completed
+var Secp256k1 *elliptic.CurveParams
+
+func init() {
+	var p, n, gx, gy big.Int
+	fmt.Sscan("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", &p)
+	fmt.Sscan("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", &n)
+	fmt.Sscan("0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", &gx)
+	fmt.Sscan("0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8", &gy)
+	b := big.NewInt(7)
+	Secp256k1 = &elliptic.CurveParams{
+		P:       &p,
+		N:       &n,
+		B:       b,
+		Gx:      &gx,
+		Gy:      &gy,
+		BitSize: 256,
+	}
+}
+
+// Calculate a shared secret using elliptic curve Diffie-Hellman
+func ECDH(priv *ecdsa.PrivateKey, pub ecdsa.PublicKey) *big.Int {
+	x, _ := Secp256k1.ScalarMult(pub.X, pub.Y, priv.D.Bytes())
+	return x
+}
